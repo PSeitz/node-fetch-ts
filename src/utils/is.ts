ddb4127507cee0cbd0e20fce860fa567d3326491
Pascal Seitz <pascal.seitz@gmail.com>
2020-01-31 19:26:29 +0100

    fix import

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { AbortError } from "../..";
 import Blob from "../blob";
+import AbortError from "../errors/abort-error";
 /**
  * Is.js
  *
